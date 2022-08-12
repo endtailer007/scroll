6443d06ded19c2e5cd34bfb43a37e62a04b8b763
endtailer <surajofficial2000@gmail.com>
2022-08-12 10:35:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scroll/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+        private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(MainActivity.this, "This button is working properly", Toast.LENGTH_SHORT).show();
+                Log.d("Sooraj","This button is working properly");
+            }
+        });
+    }
+}
